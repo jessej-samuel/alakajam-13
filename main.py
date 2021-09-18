89d6a95cef9ccc785c9784133eed828a491c0dbc
jessej-samuel <1234jessej@gmail.com>
2021-09-18 16:52:00 +0530

    Feat: Code cleanup

--- main.py
+++ main.py
@@ -10,38 +10,39 @@ class Game(object):
     HEIGHT = 480
     FPS = 30
 
-    # Define Colors 
+    # Define Colors
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
     BLUE = (0, 0, 255)
 
-    def __init__(self,gameStates = []) -> None:
-        ## initialize pygame and create window
+    def __init__(self, gameStates=[]) -> None:
+        # initialize pygame and create window
         pygame.init()
-        pygame.mixer.init()  ## For sound
+        pygame.mixer.init()  # For sound
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption("Bot Invasion")
-        self.clock = pygame.time.Clock()     ## For syncing the FPS
+        self.clock = pygame.time.Clock()  # For syncing the FPS
         self.running = True
-        self.font = pygame.font.Font("assets/pixel_reg.ttf",32)
+        self.font = pygame.font.Font("assets/pixel_reg.ttf", 32)
         self.game_states = gameStates
 
         # print("Game Initialized")
         pass
-    
+
     def handle_events(self):
-            # print("i am game events >:)")
-            for event in pygame.event.get():        # gets all the events which have occured till now and keeps tab of them.
-                ## listening for the the X button at the top
-                if event.type == pygame.QUIT:
-                    self.running = False
-                if event.type == MOUSEBUTTONDOWN:
-                    self.keys_pressed = pygame.mouse.get_pressed()
-            self.mouse_pos = pygame.mouse.get_pos()
-            # print("Game Events handled")
-    
+        # print("i am game events >:)")
+        # gets all the events which have occured till now and keeps tab of them.
+        for event in pygame.event.get():
+            # listening for the the X button at the top
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == MOUSEBUTTONDOWN:
+                self.keys_pressed = pygame.mouse.get_pressed()
+        self.mouse_pos = pygame.mouse.get_pos()
+        # print("Game Events handled")
+
     def update(self):
         # print("Game Updated")
         pass
@@ -49,14 +50,15 @@ class Game(object):
     def draw(self):
         self.screen.fill(self.BLACK)
         # print("Game Drawn")
-    
+
     def run(self):
-            # print("--------------------")
-            self.clock.tick(self.FPS)
-            self.handle_events()
-            self.update()
-            self.draw()
-            pygame.display.update()
+        # print("--------------------")
+        self.clock.tick(self.FPS)
+        self.handle_events()
+        self.update()
+        self.draw()
+        pygame.display.update()
+
 
 class MainScreen(Game):
 
@@ -64,25 +66,30 @@ class MainScreen(Game):
         super().__init__()
         self.gotonext = False
 
-        #Rendering stuff
-        self.title_text = self.font.render("Main Screen",False,self.WHITE)
+        # Rendering stuff
+        self.title_text = self.font.render("Main Screen", False, self.WHITE)
         self.title_image = pygame.image.load("assets/TITLE.png").convert()
-        self.title_image.set_colorkey((0,0,0))
-        self.start_prompt = self.font.render("Press any key to start...",False,self.WHITE)
+        self.title_image.set_colorkey((0, 0, 0))
+        self.yoff = 0
+        self.change = 0.5
+        self.start_prompt = self.font.render(
+            "Press any key to start...", False, self.WHITE)
+        self.start_prompt_rect = self.start_prompt.get_rect(
+            center=(int(self.WIDTH/2), int(self.HEIGHT/2)))
 
         # Music
-        pygame.mixer.music.load("assets/break01.ogg")
+        pygame.mixer.music.load("assets/tb303_01.ogg")
         pygame.mixer.music.set_volume(0.01)
         pygame.mixer.music.play(-1)
         # print("MainScreen here!")
         pass
 
-    
     def handle_events(self):
-        for event in pygame.event.get():        # gets all the events which have occured till now and keeps tab of them.
-            ## listening for the the X button at the top
+        # gets all the events which have occured till now and keeps tab of them.
+        for event in pygame.event.get():
+            # listening for the the X button at the top
             if event.type == pygame.QUIT:
-                    self.running = False
+                self.running = False
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     self.running = False
@@ -94,14 +101,16 @@ class MainScreen(Game):
         # print("MainScreen Events handled")
 
     def update(self):
+        # self.yoff += self.change
         pass
-    
+
     def draw(self):
         self.screen.fill(self.BLACK)
-        self.screen.blit(self.title_text,(2,2))
-        self.screen.blit(self.title_image,(50,50))
-        self.screen.blit(self.start_prompt,self.start_prompt.get_rect(center=(int(self.WIDTH/2),int(self.HEIGHT/2))))
+        self.screen.blit(self.title_text, (2, 2))
+        self.screen.blit(self.title_image, (50, 50))
+        self.screen.blit(self.start_prompt, self.start_prompt_rect)
         # print("MainScreen Drawn")
+
 
 class PlayScreen(Game):
 
@@ -109,17 +118,18 @@ class PlayScreen(Game):
         super().__init__()
         self.gotonext = False
         # Music
-        pygame.mixer.music.load("assets/break02.ogg")
+        pygame.mixer.music.load("assets/techno_bass02.ogg")
         pygame.mixer.music.set_volume(0.01)
         pygame.mixer.music.play(-1)
         # print("Playscreen here!")
         pass
-    
+
     def handle_events(self):
-        for event in pygame.event.get():        # gets all the events which have occured till now and keeps tab of them.
-            ## listening for the the X button at the top
+        # gets all the events which have occured till now and keeps tab of them.
+        for event in pygame.event.get():
+            # listening for the the X button at the top
             if event.type == pygame.QUIT:
-                    self.running = False
+                self.running = False
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     self.running = False
@@ -131,12 +141,13 @@ class PlayScreen(Game):
         # print("MainScreen Events handled")
 
     def update(self):
-        self.title_text = self.font.render("Play Screen",False,self.WHITE)
+        self.title_text = self.font.render("Play Screen", False, self.WHITE)
 
     def draw(self):
-        self.screen.fill((0,0,200))
-        self.screen.blit(self.title_text,(2,2))
+        self.screen.fill((0, 0, 200))
+        self.screen.blit(self.title_text, (2, 2))
         # print("MainScreen Drawn")
+
 
 class EndScreen(Game):
 
@@ -149,12 +160,13 @@ class EndScreen(Game):
         pygame.mixer.music.set_volume(0.01)
         pygame.mixer.music.play(-1)
         pass
-    
+
     def handle_events(self):
-        for event in pygame.event.get():        # gets all the events which have occured till now and keeps tab of them.
-            ## listening for the the X button at the top
+        # gets all the events which have occured till now and keeps tab of them.
+        for event in pygame.event.get():
+            # listening for the the X button at the top
             if event.type == pygame.QUIT:
-                    self.running = False
+                self.running = False
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     self.running = False
@@ -166,17 +178,19 @@ class EndScreen(Game):
         # print("EndScreen Events handled")
 
     def update(self):
-        self.title_text = self.font.render("End Screen",False,self.WHITE)
+        self.title_text = self.font.render("End Screen", False, self.WHITE)
 
     def draw(self):
-        self.screen.fill((200,0,0))
-        self.screen.blit(self.title_text,(2,2))
+        self.screen.fill((200, 0, 0))
+        self.screen.blit(self.title_text, (2, 2))
         # print("EndScreen Drawn")
 
+
 class GameState:
-    def __init__(self,state:str,new:bool = True):
+    def __init__(self, state: str, new: bool = True):
         self.state = state
         self.new = new
+
     def getState(self):
         if "main" in self.state:
             return MainScreen()
@@ -186,7 +200,8 @@ class GameState:
             return EndScreen()
 
 
-gamestates = [GameState("mainscreen"),GameState("playscreen"),GameState("endscreen")]
+gamestates = [GameState("mainscreen"), GameState(
+    "playscreen"), GameState("endscreen")]
 state_id = 0
 game = gamestates[state_id].getState()
 while game.running:
